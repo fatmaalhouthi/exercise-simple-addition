08ab131130f94ab587736bf76b47ae3640b9babd
Fatma Alhouthi <fatma1270@gmail.com>
2017-09-07 14:50:37 +0200

    deleted console.log(addNumbers()) from 'clac' file

--- src/calc.js
+++ src/calc.js
@@ -17,17 +17,7 @@ function addNumbers () {
   let firstNumber = 12
   let secondNumber = 30
 
-<<<<<<< HEAD
-
   return (firstNumber+secondNumber)
 }
-  
-=======
->>>>>>> d21e8698a17a855cb7ef188d686a48ed0f7d67c4
 
-  return (firstNumber+secondNumber)
-  
-  
-}
 exports.addNumbers = addNumbers
-console.log(addNumbers())
